b8d2b9b135fa16e8b01645952b8317f24a3c2291
KirillSmirnov <smirnowku@gmail.com>
2025-10-05 23:20:34 +0300

    Publishing

--- authorized-websocket-spring-boot-starter/build.gradle.kts
+++ authorized-websocket-spring-boot-starter/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    `maven-publish`
     id("io.freefair.lombok") version "9.0.0"
     id("org.springframework.boot") version "3.5.6"
 }
@@ -21,4 +22,26 @@ tasks.withType<Test> {
 
 tasks.bootJar {
     enabled = false
+}
+
+tasks.withType<GenerateModuleMetadata>().configureEach {
+    suppressedValidationErrors.set(setOf("dependencies-without-versions"))
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/KirillSmirnow/authorized-websocket-spring-boot-starter")
+            credentials {
+                username = "KirillSmirnow"
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>("github") {
+            from(components["java"])
+        }
+    }
 }
